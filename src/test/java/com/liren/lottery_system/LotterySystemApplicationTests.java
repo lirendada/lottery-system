3a9df71fa83c25ba5a448dfd0d3a2a937a89c3d5
lirendada <2916776007@qq.com>
2025-06-27 17:26:45 +0800

    搭建项目框架

---
+++ src/test/java/com/liren/lottery_system/LotterySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liren.lottery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotterySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
